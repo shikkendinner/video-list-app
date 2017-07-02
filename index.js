0d9995523402602e3abf33bce67c975956e3ec0f
ali.azhar <ali.azhar@mail.utoronto.ca>
2017-07-02 04:49:26 -0400

    Added contents of the video database to index.html, videos may not load due to overload of websockets, currently still need to add a button to control which video starts up and hide the descriptions, also added index.css for future css modifications

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var express = require("express");
 var app = express();
 
+//utilize the request package to make HTTP requests 
+var request = require("request");
+
 //defines what to do when receiving a get request for route "localhost:3000/"
 //returns the index.html file
 app.get("/", function(req, res){
@@ -16,6 +19,21 @@ app.get("/", function(req, res){
   });
 });
 
+//http request is performed to retrieve the rss feed which is returned to be processed by the frontend
+app.get("/getRSS", function(req, res){
+  var options = {
+    method: "GET",
+    uri: "https://s.ch9.ms/Feeds/RSS"
+  };
+  request(options, function(error, response, body){
+    if(error) res.status(400).send(error);
+    if(response.statusCode == 200) {
+      res.set("Content-Type", "text/xml");
+      res.send(body);
+    }
+  });
+});
+
 //this serves the javascript static file app.js
 app.get("/javascript/app.js", function(req, res){
   res.sendFile("app.js", {root: __dirname+"/javascript/"}, function(err){
@@ -25,6 +43,18 @@ app.get("/javascript/app.js", function(req, res){
       res.status(404).send("File not found!");  
     }else {
       console.log("app.js found and sent!");
+    }
+  });
+});
+
+//this serves the css static file index.css
+app.get("/css/index.css", function(req,res){
+  res.sendFile("index.css", {root: __dirname+"/css/"}, function(err){
+    //check whether there is an error with getting the file
+    if(err){
+      res.status(404).send("File not found!");
+    }else {
+      console.log("index.css found and sent!");
     }
   });
 });
--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,19 @@
 //this module defines the whole application
 var app = angular.module("vlApp", []);
 
+//this configuration allows the acceptance of loading URLs from outside the domain on which this application resides, which by default rejects any and all urls outside of this domain
+app.config(function($sceDelegateProvider){
+  //the urls that are allowed
+  $sceDelegateProvider.resourceUrlWhitelist([
+    //allow all links with the same origins
+    "self",
+    //allow all links with the ch9 origin for videos
+    "http://video.ch9.ms/ch9/**"
+  ]);
+});
+
 //this controller controls the whole application
-app.controller("vlCtrl", function($scope){
+app.controller("vlCtrl", function($scope, $http){
   /*
   * The format of this database will be as shown below:
   * videoDB = {"title of video1": {"url":"string", "description":"string", "tags":[string, string]}, "title of video2": ...}
@@ -13,30 +24,66 @@ app.controller("vlCtrl", function($scope){
   * The format of this database will be as shown below:
   * tagDB = {"tag1":[list of video titles], "tag2":[...]}
   */
-  $scope.tagDB = {};
+  var tagDB = {};
   
-  //populate the video database
-  populateVideoDB($scope);
-  
+  //populate the video & tag database
+  populateDatabases($scope, tagDB, $http);
 });
 
-function populateVideoDB($scope){
-  //sends the request for the local file (asynchronously)
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "https://s.ch9.ms/Feeds/RSS", true);
-  
-  //this is run when the request returns with content from the source
-  xhttp.onload = function(){
-    if(this.readyState == 4 && this.status == 200){
+function populateDatabases($scope, tagDB, $http){
+  //sends the request for the rss feed file (asynchronously)
+  $http({
+    method:"GET", 
+    url:"/getRSS"
+  }).then(function successCallback(response){
+      //this is run when the request returns with content from the source
       //parse the xml file
       console.log("The file was retrieved!");
-    }
-  };
-  
-  //this is run when the request encounters an error and cannot get the content
-  xhttp.onerror = function(){
-    console.error("The file could not be retrieved. It returned with the following error: " + (this.statusText===""?"Cross-Origin Error":this.statusText));
-  };
-  
-  xhttp.send();
+
+      //use the XML DOM parser to parse the response data text
+      var parser = new DOMParser();
+      var xmlDoc = parser.parseFromString(response.data, "text/xml");
+
+      //get the list of <item> tag elements which contain information for each article within the RSS feed
+      //store information per video found in videoDB
+      var rssXML = xmlDoc.getElementsByTagName("item");
+      for(var i=0; i<rssXML.length; i++){
+        var video = rssXML[i];
+
+        //check whether a <enclosure> tag exists, which probably indicates a video file (rarely a mp3 file)
+        var enclosure = video.getElementsByTagName("enclosure");
+        if(enclosure.length !== 0){
+          //get the title of the video and store it within the database with an empty object
+          var title = video.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+          $scope.videoDB[title] = {};
+
+          //get the url to the mp4 file of the video
+          $scope.videoDB[title].url = enclosure[0].attributes.getNamedItem("url").nodeValue;
+
+          //get the description for the video
+          $scope.videoDB[title].description = video.getElementsByTagName("summary")[0].childNodes[0].nodeValue;
+
+          //get the tags for the video, and store this video's title for each tag within the tag database
+          //if a video does not have a tag, it will not be in the tag database
+          var tags = video.getElementsByTagName("category");
+          $scope.videoDB[title].tags = [];
+
+          for(var j=0; j<tags.length; j++){
+            var tagName = tags[j].childNodes[0].nodeValue;
+
+            //add tag to video database tag list for this particular video
+            $scope.videoDB[title].tags.push(tagName);
+
+            //check if tag exists within tag database, in either case, add this video's title to the tag database
+            if(!tagDB.hasOwnProperty(tagName)){
+              tagDB[tagName] = [];
+            }
+            tagDB[tagName].push(title);
+          }
+        }
+      }
+    }, function errorCallback(response){
+        //this is run when the request encounters an error and cannot get the content
+        console.error("The file could not be retrieved. It returned with the following error: " + (response.statusText===""?"Cross-Origin Error":response.statusText));
+    });
 }
